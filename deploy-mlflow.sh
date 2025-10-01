8f1b40b46116628ecb4cf62be80842eccebb9611
Ignace <edwardoignace928@gmail.com>
2025-10-02 00:17:29 +0200

    version 1.0

---
+++ deploy-mlflow.sh
@@ -0,0 +1,8 @@
+# ==================== MLflow PostgreSQL Configuration ====================
+MLFLOW_POSTGRES_USER=mlflow
+MLFLOW_POSTGRES_PASSWORD=mlflow123
+MLFLOW_POSTGRES_DB=mlflow
+
+# ==================== MLflow MinIO Configuration ====================
+MLFLOW_MINIO_USER=mlflow_admin
+MLFLOW_MINIO_PASSWORD=mlflow_admin123
